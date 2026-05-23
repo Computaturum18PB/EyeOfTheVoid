1ecf161c083aa83b3298be75346d419f35c0d07c
Computaturum18PB <ismaklakov@gmail.com>
2026-05-23 18:38:26 +0300

    Добавление загрузочного экрана перед основным окном

---
+++ src/core/load.py
@@ -0,0 +1,23 @@
+from PySide6.QtWidgets import QLabel, QProgressBar, QVBoxLayout, QWidget
+from PySide6.QtCore import Qt
+
+class LoadWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(500, 300)
+        
+        self.progress = QProgressBar()
+        self.progress.setRange(0, 0)
+        self.progress.setFixedWidth(300)
+        
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignCenter)
+        
+        self.label = QLabel("Загрузка")
+        self.label.setAlignment(Qt.AlignCenter)
+        
+        layout.addWidget(self.label)
+        layout.addWidget(self.progress)
+        
+        self.setLayout(layout)
+        
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from PySide6.QtWidgets import QApplication
 from core.core import CoreWindow
+from core.load import LoadWindow
+import time
 
 def main():
     app = QApplication([])
+    
+    load = LoadWindow()
+    load.show()
+    app.processEvents()
+
     window = CoreWindow()
+    
+    load.close()
     window.show()
     app.exec()
     
